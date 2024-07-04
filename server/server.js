9848f2fe35b4eca50fd910e9173c9d8ecb57521b
Alex <adlam4002@hotmail.co.uk>
2024-07-04 11:54:06 +0100

    basic setup for server and client

--- server/server.js
+++ server/server.js
@@ -15,3 +15,29 @@ const port = 8080;
 app.listen(port, () => {
   console.log(`Your server is running on port: ${port}`);
 });
+app.get("/", (req, res) => {
+  res.json({ Message: "Root route wokring!" });
+});
+
+// STRETCH GOAL
+//update
+app.put("/updateformdata/:id", async (req, res) => {
+  const dataId = req.params.id;
+  const result = await database.query(
+    `
+  UPDATE tablename set column_name = $1, second_column = $2 WHERE id = $3 RETURNING *`,
+    [data_one, data_two, dataId]
+  );
+  res.json(result.rows);
+});
+//delete
+app.delete("/deleteformdata/:id", async (req, res) => {
+  const dataId = rreq.param.id;
+  const result = await database.query(
+    `
+  DELETE FROM table_name WHERE id = $1 RETURNING *
+  `,
+    [dataId]
+  );
+  res.json(result.rows);
+});
