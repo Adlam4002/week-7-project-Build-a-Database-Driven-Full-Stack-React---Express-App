2ab0a2f6d6af395e35637554d4c89c83d6f519b9
Alex <adlam4002@hotmail.co.uk>
2024-07-04 13:59:57 +0100

    basic components created

---
+++ client/src/pages/Form.jsx
@@ -0,0 +1,19 @@
+export default function Form() {
+  event.preventDefault();
+  let formData = {
+    key: value,
+    key: value,
+  };
+  function handleSubmit() {
+    fetch("url", {
+      method: "",
+      body: "",
+      headers: { "Content/type": "application/json" },
+    });
+  }
+  function handleChange() {}
+  // have an on change so the comments will be previewed, handle the change of user input
+  // the key is the target name, the value is the target value
+  return;
+  //   need two events, one to submit and one to track changes
+}
---
+++ client/src/pages/Posts.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export default function Posts() {
+  //  need state to save the values of posts
+  // need useEffect to fetch the data
+  useEffect();
+  // function to get the posts
+  // function is async and uses fetch
+  // once you fetch the data, you will set the state variable to bne the posts data
+  return (
+    <>
+      <h1>Posts</h1>
+    </>
+  );
+}
