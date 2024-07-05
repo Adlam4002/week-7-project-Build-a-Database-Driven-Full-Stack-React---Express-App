0de352347b9cf455371aaba488c7168ffd2e82b1
Alex <adlam4002@hotmail.co.uk>
2024-07-05 14:57:40 +0100

    moving delete button

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -31,13 +31,13 @@ export default function Review({
   return (
     <>
       <div id="review-box" key={review_id}>
-        <h4 id="review-username">
-          User: {username} id {review_id}
-        </h4>{" "}
+        <h4 id="review-username">User: {username}</h4>{" "}
         <h4 id="review-aname">{anime_name}</h4>{" "}
         <h4 id="review-score">Score: {score}/5</h4>{" "}
         <p id="review-comment">Review: {comment}</p>
-        <button onClick={handleDelete}>Delete</button>
+        <button id="delbut" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </>
   );
