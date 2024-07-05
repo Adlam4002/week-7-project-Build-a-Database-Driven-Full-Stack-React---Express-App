9010247aef9c3630931f54c1304685aff1abb4b5
Alex <adlam4002@hotmail.co.uk>
2024-07-05 14:50:08 +0100

    more del test

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -28,7 +28,9 @@ export default function Review({
   return (
     <>
       <div id="review-box" key={review_id}>
-        <h4 id="review-username">User: {username}</h4>{" "}
+        <h4 id="review-username">
+          User: {username} id {review_id}
+        </h4>{" "}
         <h4 id="review-aname">{anime_name}</h4>{" "}
         <h4 id="review-score">Score: {score}/5</h4>{" "}
         <p id="review-comment">Review: {comment}</p>
