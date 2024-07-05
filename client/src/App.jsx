7b7a80910c16b595bd4f1eebe0c4bac5fb95496b
Alex <adlam4002@hotmail.co.uk>
2024-07-05 13:29:45 +0100

    added to home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Form from "./pages/Form";
 import { Route, Routes } from "react-router-dom";
 import Posts from "./pages/Posts";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/reviews" element={<Posts />}></Route>
         <Route path="/submit-a-review" element={<Form />} />
       </Routes>
