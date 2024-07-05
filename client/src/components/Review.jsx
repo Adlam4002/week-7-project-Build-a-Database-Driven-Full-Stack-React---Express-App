fef3eef24871baf3d30212261f8b0a75a8479d34
Alex <adlam4002@hotmail.co.uk>
2024-07-05 14:04:30 +0100

    delete button testing

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -4,7 +4,27 @@ export default function Review({
   comment,
   score,
   anime_name,
+  onDelete,
 }) {
+  async function handleDelete() {
+    try {
+      const response = await fetch(
+        "https://week-7-project-server.onrender.com/deleteformdata/${review_id}",
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await response.json();
+      if (data.success) {
+        console.log("Deleted");
+        onDelete(review_id);
+      } else {
+        console.error("failed to delete", data.error);
+      }
+    } catch (error) {
+      console.error("Delete error", error);
+    }
+  }
   return (
     <>
       <div id="review-box" key={review_id}>
@@ -12,6 +32,7 @@ export default function Review({
         <h4 id="review-aname">{anime_name}</h4>{" "}
         <h4 id="review-score">Score: {score}/5</h4>{" "}
         <p id="review-comment">Review: {comment}</p>
+        <button onClick={handleDelete}>Delete</button>
       </div>
     </>
   );
