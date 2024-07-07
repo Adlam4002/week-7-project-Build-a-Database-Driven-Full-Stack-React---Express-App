9d419e277a217b634f61e4e08c8cc0fb719f4d4e
Alex <adlam4002@hotmail.co.uk>
2024-07-07 08:46:50 +0100

    Home page

---
+++ client/src/components/Recommendation.jsx
@@ -0,0 +1,17 @@
+export default function Recommendation() {
+  return (
+    <>
+      <h3>Recommendation for the month: Hell&apos;s Paradise</h3>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/aaHZdwPZX6g?si=oMFY-0SP_-dVuRgP"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </>
+  );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import Recommendation from "../components/Recommendation";
+
 export default function home() {
   return (
     <>
       <h1 id="home">Welcome to Terror&apos;s Anime Reviews</h1>
       <h2>The UK&apos;s number 1 anime discussion board</h2>
       <h3>Feel free to browse our reviews or even add one yourself!</h3>
+      <Recommendation />
     </>
   );
 }
